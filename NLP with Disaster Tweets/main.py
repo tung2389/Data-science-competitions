96f6650c83ed7f689ad95bc9adb39447b38c6632
unknown <luukhanhtung2002@gmail.com>
2020-04-05 16:43:07 +0700

    Update README

--- NLP with Disaster Tweets/main.py
+++ NLP with Disaster Tweets/main.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.layers import Input, Embedding, LSTM, Flatten, Dropout, Dense, MaxPooling1D
+from tensorflow.keras.layers import Input, Embedding, LSTM, Flatten, Dropout, Dense, MaxPooling1D, Bidirectional, Conv1D, SpatialDropout1D
 from tensorflow.keras.models import Model
 import pandas as pd
 import numpy as np
@@ -49,19 +49,23 @@ keyAndLocInput = np.asarray(np.concatenate((keyInput, locInput), axis=1), dtype=
 # num_words + 1 because the 0 position is 'UNK' words
 
 text_input_layer = Input(shape=(maxLen,), name = 'text_input')
-embedding_layer_1 = Embedding(input_dim = num_words + 2, output_dim = 200, input_length = maxLen)(text_input_layer)
-lstm_layer = LSTM(150, return_sequences=True)(embedding_layer_1)
-lstm_layer = Dropout(0.2)(lstm_layer)
-pooling_layer = MaxPooling1D(pool_size=3)(lstm_layer)
-flatten_pooling = Flatten()(pooling_layer)
+embedding_layer_1 = Embedding(input_dim = num_words + 2, output_dim = 16, input_length = maxLen)(text_input_layer)
+lstm_layer = Bidirectional(LSTM(10, return_sequences=True))(embedding_layer_1)
+# lstm_layer = SpatialDropout1D(0.3)(lstm_layer)
+convolution_layer = Conv1D(10, kernel_size=4)(lstm_layer)
+# lstm_layer = Dropout(0.2)(lstm_layer)
+# pooling_layer = MaxPooling1D(pool_size=2)(convolution_layer)
+flatten_pooling = Flatten()(convolution_layer)
 
 other_input_layer = Input(shape=(2,), name = 'other_input')
 embedding_layer_2 = Embedding(input_dim = num_key_words + num_locations + 2, output_dim = 2, input_length = 2)(other_input_layer)
 flatten_embedding = Flatten()(embedding_layer_2)
 concatenated_layer = keras.layers.concatenate([flatten_pooling, flatten_embedding])
 
-next_layer = Dense(256, activation='relu')(concatenated_layer)
-next_layer = Dense(128, activation='relu')(next_layer)
+next_layer = Dense(16, activation='relu')(concatenated_layer)
+# next_layer = Dropout(0.2)(next_layer)
+# next_layer = Dense(128, activation='relu')(next_layer)
+# next_layer = Dropout(0.2)(next_layer)
 # next_layer = Dense(64, activation='relu')(next_layer)
 
 output_layer = Dense(1, activation='sigmoid', name='output')(next_layer)
---
+++ README.md
@@ -0,0 +1,1 @@
+- My solutions for some machine learning problems in Kaggle
